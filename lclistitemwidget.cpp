80887bca3d7471513f16c32e94f8677b768a8bbc
adolph <497749990@qq.com>
2019-03-28 00:27:23 +0800

    2019/03/28 初步完成

---
+++ lclistitemwidget.cpp
@@ -0,0 +1,6 @@
+#include "lclistitemwidget.h"
+
+LCListItemWidget::LCListItemWidget(QWidget *parent) : QWidget(parent)
+{
+
+}
---
+++ lclistitemwidget.h
@@ -0,0 +1,17 @@
+#ifndef LCLISTITEMWIDGET_H
+#define LCLISTITEMWIDGET_H
+
+#include <QWidget>
+
+class LCListItemWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit LCListItemWidget(QWidget *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // LCLISTITEMWIDGET_H
